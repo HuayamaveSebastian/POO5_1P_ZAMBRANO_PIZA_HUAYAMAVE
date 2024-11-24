ec46c5507a575f7c1c6df50af154d1a746e746d2
AbrahamPizaO <abrapiza@espol.edu.ec>
2024-11-24 15:13:14 -0500

    Usuario.java

--- demo/src/main/java/com/example/Usuario.java
+++ demo/src/main/java/com/example/Usuario.java
@@ -1,9 +1,10 @@
 package com.example;
 
-public class Usuario {
+abstract class Usuario {
     protected String nombre;
     protected String apellido;
     protected int numCedula;
+    protected int codigoUnico;
     protected String usuario;
     protected String contrasenia;
     protected String correo; 
@@ -20,16 +21,83 @@ public class Usuario {
 
     }
 
-    protected void gestionarReserva (String fecha){
+    public abstract void gestionarReserva (String fecha){
 
     }
 
     protected void enviarCorreo(String profesor,Enum tipoEspacio,String materia ){
+        System.out.println("Solicitud en proceso, se enviará un correo a profesor:" + profesor.getCorreo());
 
     }
 
     protected void enviarCorreo(String estudiante,Enum tipoEspacio, int matricula){
+        System.out.println("Enviando correo a estudiante:" + estudiante.getCorreo());
 
     }
+
+    public String getNombres(){
+        return nombres;
+    }
+    
+    public void setNombres(String nombres){
+        this.nombres = nombres;
+    }
+
+    public String getApellido(){
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos){
+        this.apellidos = apellidos;
+    }
+
+    public int getNumCedula(){
+        return numCedula;
+    }
+
+    public void setNumCedula(int numCedula){
+        this.numCedula = numCedula;
+    }
+
+    public getCodigoUnico(){
+        return codigoUnico;
+    }
+
+    public void setCodigoUnico(int codigoUnico){
+        this.codigoUnico = codigoUnico;
+    }
+
+    public String getUsuario(){
+        return usuario;
+    }
+
+    public void setUsuario(String usuario){
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia(){
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia){
+        this.contraenia = contrasenia;
+    }
+
+    public String getCOrreo(){
+        return correo;
+    }
+
+    public setCorreo(String correo){
+        this.correo = correo;
+    }
+
+        
+        
+        
+
+
+
+
+    
 }
 

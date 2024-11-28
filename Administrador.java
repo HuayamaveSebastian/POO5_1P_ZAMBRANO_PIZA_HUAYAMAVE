bd3cfa41dc5840931b1a93d5a40293a54950c7ca
AbrahamPizaO <abrapiza@espol.edu.ec>
2024-11-28 17:21:35 -0500

    Update Administrador.java

--- Administrador.java
+++ Administrador.java
@@ -11,20 +11,47 @@ public class Administrador extends Usuario {
         this.cargo = cargo;
 
     }
+
+    public cargo getCargo(){
+        return cargo;
+    }
+
+    public void setCargo(){
+        this.cargo = cargo;
+    }
+
     
     public void gestionarReserva(List<Reserva> reservas) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Reservas pendientes:");
         for (Reserva reserva : reservas) {
-            if (reserva.getEstado().equals("PENDIENTE")) {
-                System.out.println(reserva);
+            if (reserva.getEstado()== Estado.Pendiente){
+                System.out.println("Codigo de reserva:" + reserva.getCodigoReserva());
+                System.out.println(reseva.toString());
+                System.out.println("Por favor, confirme la solicitud de reserva... (1: Aprobar, 2: Rechazar)");
+
+                int decision = scanner.nextInt();
+                scanner.nextLine(); 
+                if (decision == 1) {
+                    reserva.setEstado(Estado.Aprobado);
+                    system.out.printlln("Su reserva ha sido Aprobada");
+                }
+                else if (decision ==2){
+                    System.out.printlln("Describa el motivo de rechazo:");
+                    String motivoRechazo = scanner.nextLine();
+                    reserva.setEstado(Estado.Rechazado);
+                    System.ouy.println("Su reserva ha sido Rechazda");
+                }
+                else{
+                    System.out.println("Opcion no valida. Continue con la sigueinte reserva");
+                }       
             }
         }
     }
     @Override  //Añadir la forma en que se consulta ya que es administrador
     public void consultarReserva(Date fecha){
-        System.out.println("Consultar Reserva");
+        System.out.println("Consultar reservas para el dia:" = fecha);
     }
 
     @Override
@@ -32,8 +59,10 @@ public class Administrador extends Usuario {
         return "Administrador{" +
                "nombre='" + nombre + '\'' +
                ", apellido='" + apellido + '\'' +
-               ", numCedula=" + numCedula +
-               ", correo='" + correo + '\'' +
+               ", numCedula=" + numCedula + '\'' +
+               ", usuario=" + usuario + '\'' +
+               ", correo='" + correo + '\'' + 
+               ", cargo=" + cargo +
                '}';
     }
 }

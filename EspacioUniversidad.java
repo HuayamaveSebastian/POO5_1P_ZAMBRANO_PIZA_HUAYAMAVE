ddbdecb83e0af4c61b5edd9e324f3091e677a8f3
AbrahamPizaO <abrapiza@espol.edu.ec>
2024-11-29 00:00:21 -0500

    Update EspacioUniversidad.java

--- EspacioUniversidad.java
+++ EspacioUniversidad.java
@@ -1,58 +1,64 @@
-import Enums.*;
+import Enums.EstadoEspacio;
+import Enums.TipoEspacio;
 
 public class EspacioUniversidad {    //falta la extension a gestionar reserva
-    private TipoEspacio tipo;
-    private int CogigounicoEspacio;
-    private int Capacidad;
+    private TipoEspacio tipoEspacio;
+    private int cogigoUnicoEspacio;
+    private int capacidad;
     private String nombre;
     private EstadoEspacio estadoEspacio;
 
-    public EspacioUniversidad(TipoEspacio tipo, int CogigounicoEspacio, int Capacidad, String nombre, EstadoEspacio estadoEspacio){
-        this.Capacidad = Capacidad;
+    public EspacioUniversidad(TipoEspacio tipo, int cogigoUnicoEspacio, int capacidad, String nombre, EstadoEspacio estadoEspacio){
+        this.capacidad = capacidad;
         this.tipo = tipo;
-        this.CogigounicoEspacio = CogigounicoEspacio;
+        this.cogigoUnicoEspacio = cogigoUnicoEspacio;
         this.nombre = nombre; 
         this.estadoEspacio = estadoEspacio; 
-    }
-
-    public void setTipo(TipoEspacio tipo){
-        this.tipo = tipo; 
-    }
-
-    public TipoEspacio getTipo(){
-        return tipo;
-    }
-
-    public void setCodigoUnicoEspacio(int CogigounicoEspacio){
-        this.CogigounicoEspacio = CogigounicoEspacio;
-    }
-
-    public int getCodigoUnicoEspacio(){
-        return CogigounicoEspacio;
-    }
-
-    public void setCapacidad(int Capacidad){
-        this.Capacidad = Capacidad;
-    }
-
-    public int getCapacidad (){
-        return Capacidad;
-    }
-
-    public void setNombre(String nombre){
-        this.nombre = nombre; 
     }
 
     public String getNombre (){
         return nombre;
     }
 
-    public void setEstadoEspacio(EstadoEspacio estadoEspacio){
-        this.estadoEspacio = estadoEspacio;
+     public void setNombre(String nombre){
+        this.nombre = nombre; 
+    }
+
+    public int getCapacidad (){
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad){
+        this.capacidad = capacidad;
+    }
+
+    public int getCodigoUnicoEspacio(){
+        return CogigounicoEspacio;
+    }
+
+    public void setCodigoUnicoEspacio(int cogigoUnicoEspacio){
+        this.cogigoUnicoEspacio = cogigoUnicoEspacio;
     }
 
     public EstadoEspacio getEstadoEspacio(){
         return estadoEspacio;
     }
+
+    public void setEstadoEspacio(EstadoEspacio estadoEspacio){
+        this.estadoEspacio = estadoEspacio;
+    }
+
+    public TipoEspacio getTipoEspacio(){
+        return tipoEspacio;
+    }
+    
+    public void setTipo(TipoEspacio tipo){
+        this.tipo = tipo; 
+    }
+
+    
+
+
+    
     
 }

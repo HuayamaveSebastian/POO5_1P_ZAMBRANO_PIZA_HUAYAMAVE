99e92e61f0d51df004bfbae001fb6f0449973218
AbrahamPizaO <abrapiza@espol.edu.ec>
2024-11-28 22:38:24 -0500

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -7,37 +7,48 @@ public class Estudiante extends Usuario{
     private int matricula;
     private String carrera; 
 
-    @Override   //aumentar aqui, cambiar.
-    public void consultarReserva( Date fecha ){
-        System.out.println("Consultando la reserva del estudiante")
-    }
-
     public Estudiante( String nombre, String apellido, String numCedula,String usuario, String contrasenia, String correo,int matricula, String carrera){
         super(nombre, apellido, numCedula, usuario, contrasenia, correo);
         this.matricula = matricula;
         this.carrera = carrera; 
     }
-
-    public void setMatricula(int matricula){
-        this.matricula = matricula;
+    
+    @Override   
+    public void consultarReserva( Date fecha ){
+        System.out.println("El Estudiante" + this.nombre + "está consultando reservas para el dia:" + fecha);
     }
 
+    @Override
+    public void gestionarReserva(Date fecha ){
+        System.out.println("El Estudiante" + this.nombre + " está modificando su reserva para el día:" + fecha);
+    }
+
+    
     public int getMatricula(){
         return matricula;
     }
-
-    public void setCarrera(String carrera){
-        this.carrera = carrera;
+    
+    public void setMatricula(int matricula){
+        this.matricula = matricula;
     }
 
     public String getCarrera (){
         return carrera; 
     }
 
-    @Override
-    public void gestionarReserva(Date fecha ){
-        System.out.println("Espere su confirmación");
-        // ponerle mas cosas 
+    public void setCarrera(String carrera){
+        this.carrera = carrera;
     }
-
+    
+    @Override
+    public String toString(){
+        return "Estudiuante{" +
+            "nombre=" + nombre + '\'' +
+            ", apellido='" + apellido + '\'' +
+                ", matricula=" + matricula +
+                ", carrera='" + carrera + '\'' +
+                ", usuario='" + usuario + '\'' +
+                ", correo='" + correo + '\'' +
+                '}';
+    }
 }   

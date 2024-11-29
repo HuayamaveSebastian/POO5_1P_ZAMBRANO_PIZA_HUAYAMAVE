7c6518f1934746cc3aab9e06c0d187c867bb578a
AbrahamPizaO <abrapiza@espol.edu.ec>
2024-11-28 22:22:39 -0500

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,6 +1,6 @@
-import java.sql.Date;
+import java.util.Date;
 
-import Enums.*;
+import Enums.TipoEspacio;
 
 public abstract class Usuario {
     protected String nombre;
@@ -11,62 +11,61 @@ public abstract class Usuario {
     protected String correo; 
 
     public Usuario(String nombre, String apellido, String numCedula, String usuario, String contrasenia, String correo){
-        this.apellido = apellido;
-        this.contrasenia = contrasenia;
-        this.correo = correo;
         this.nombre = nombre;
+        this.apellido = apellido;
         this.numCedula = numCedula;
         this.usuario = usuario;
+        this.contrasenia = contrasenia;
+        this.correo = correo;
     }
 
-    abstract void consultarReserva(Date fecha); //Este deberia ir como abstract debido a que se repite en los tres pero funciona diferente
+    public abstract void consultarReserva(Date fecha); //Este deberia ir como abstract debido a que se repite en los tres pero funciona diferente
 
-    abstract void gestionarReserva (Date fecha){
+    public abstract void gestionarReserva (Date fecha){
 
     }
 
     protected void enviarCorreo(String correoprofesor, TipoEspacio tipo,String materia ){
-        System.out.println("Solicitud en proceso, se enviará un correo a profesor:" + correoprofesor);
-        System.out.println("Tipo de espacio: " + tipo + ", Materia: " + materia);
+        System.out.println("Solicitud en proceso, se enviará un correo al profesor:" + correoprofesor);
+        System.out.println("De: " + this.correo);
+        System.out.println("Asunto: Notificacion de Reserva");
+        System.out.println("Mensaje:");
+        System.out.println("Se solicitó el uso del siguiente espacio:" + tipo);
+        System.out.println("Materia relacionada:" + materia);
+        System.out.println("Por favor, revise y gestione dicha solicitud");
 
     }
     protected void enviarCorreo(String destinatario, String cuerpo) {
+        System.out.println("Sistema de envios de correo en proceso");
         System.out.println("De: " + this.correo);
         System.out.println("Para: " + destinatario);
-        System.out.println("Asunto: Reserva Realizada");
-        System.out.println("Mensaje: " + cuerpo);
-    }
-    
-    public void setNombre(String nombre){
-        this.nombre = nombre;
+        System.out.println("Asunto: Notificacion del sistema");
+        System.out.println("Mensaje: ");
+        System.out.println(cuerpo);
     }
 
     public String getNombre(){
         return nombre;
     }
 
-    public void setApellidos(String apellido){
-        this.apellido = apellido;
+    public void setNombre(String nombre){
+        this.nombre = nombre;
     }
 
     public String getApellido(){
         return apellido;
-
     }
+
+    public void setApellidos(String apellido){
+        this.apellido = apellido;
+    }
+    
     public String getNumCedula(){
         return numCedula;
     }
 
     public void setNumCedula(String numCedula){
         this.numCedula = numCedula;
-    }
-
-    public int getCodigoUnico(){
-        return codigoUnico;
-    }
-
-    public void setCodigoUnico(int codigoUnico){
-        this.codigoUnico = codigoUnico;
     }
 
     public String getUsuario(){
